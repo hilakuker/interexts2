f2b6d4379912a8b7c0c506e623b2653e9eff0417
sashapol <sashapoliacov@gmail.com>
2014-08-03 19:43:50 +0300

    Event wall form

--- Interext/Interext/Controllers/HomeController.cs
+++ Interext/Interext/Controllers/HomeController.cs
@@ -34,21 +34,66 @@ namespace Interext.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(string FreeText)
+        public ActionResult Index(string FreeText, string locationSearchTextField, string DateOfTheEventFrom,
+            string DateOfTheEventTo, string AgeOfParticipant, string Gender)
         {
-            List<Event> model=null;
+            List<Event> model = null;
+
+            bool isFreeText = !String.IsNullOrEmpty(FreeText);
+            bool isLocation = !String.IsNullOrEmpty(locationSearchTextField);
+            bool isFromDate = false;
+            bool isToDate = false;
+            bool isParticipantAge = false;
+
+            DateTime DateFrom = DateTime.MinValue;
+            if (!String.IsNullOrEmpty(DateOfTheEventFrom))
+            {
+                isFromDate = DateTime.TryParse(DateOfTheEventFrom, out DateFrom);
+            }
+
+            DateTime DateTo = DateTime.MaxValue;
+            if (!String.IsNullOrEmpty(DateOfTheEventTo))
+            {
+                isToDate = DateTime.TryParse(DateOfTheEventTo, out DateTo);
+            }
+
+            int participantAge = 0;
+            if (!String.IsNullOrEmpty(AgeOfParticipant))
+            {
+                isParticipantAge = int.TryParse(AgeOfParticipant, out participantAge);
+            }
+
             if (ModelState.IsValid)
             {
                 //if (Request.IsAjaxRequest())
                 //{
-                    model = _db.Events.Where(x=>x.Title.ToLower().Contains(FreeText.ToLower())).ToList(); // temp
-                    return PartialView("~/Views/Event/_EventsWall.cshtml", model);
+                model = _db.Events.Where(x => (isFreeText ? x.Title.ToLower().Contains(FreeText.ToLower()) || x.Description.ToLower().Contains(FreeText.ToLower()) : true)
+                    && (isLocation ? (x.Place.ToLower() == locationSearchTextField.ToLower()) : true)
+                    && (isFromDate ? (x.DateTimeOfTheEvent >= DateFrom) : true)
+                    && (isToDate ? (x.DateTimeOfTheEvent <= DateTo) : true)
+                    && (isParticipantAge ? (x.AgeOfParticipantsMin <= participantAge) : true)
+                    && (isParticipantAge ? (x.AgeOfParticipantsMax.HasValue? x.AgeOfParticipantsMax >= participantAge: true) : true)
+                    && x.GenderParticipant == Gender
+                    ).ToList(); // temp
+                return PartialView("~/Views/Event/_EventsWall.cshtml", model);
                 //}   
             }
             return View(model);
         }
 
+        private void checkRenge(int x, int y, int a, int b)
+        {
+            
+            int min = x;
+            int max = y;
+            if (a < x)
+                min = a;
+            if (b > y)
+                max = b;
 
+           
+
+        }
 
         public ActionResult About()
         {
