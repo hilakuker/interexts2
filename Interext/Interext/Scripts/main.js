80f5916d032c251339ec5036bf608bbaec6b6925
sashapol <sashapoliacov@gmail.com>
2014-08-05 19:50:02 +0300

    Select interests

--- Interext/Interext/Scripts/main.js
+++ Interext/Interext/Scripts/main.js
@@ -56,12 +56,49 @@
                 $(hiddenSection).fadeOut();
                 dataForPopupElement.html(popupContentElement.html());
             });
+
+            $(".main-category").each(function () { 
+                $(this).on("click", function (event) {
+                    event.stopPropagation();
+                    var allCatInterests = $(this).find("input.allCategoryInterests");
+                    var ifIsChecked = $(allCatInterests).prop("checked");
+                    console.log(ifIsChecked);
+                    if (ifIsChecked == false) {
+                        $(allCatInterests).prop("checked", true);
+                    }
+                    else if (ifIsChecked == true) {
+                        $(allCatInterests).prop("checked", false);
+                    }
+                });
+            });
+
+            $(".main-category input").on("click", function (event) {
+                event.stopPropagation();
+            });
+
+                
+
+
             $(".hidden-popup-container").one("click", function () {
                 $(hiddenSection).fadeOut();
                 dataForPopupElement.html(popupContentElement.html());
             });
         });
     });
+
+    
+
+    //$(".interests-list .main-category").on("click", function () {
+    //    console.log("hh");
+    //    if ($(this).find(".checkbox").hasClass("fa-square-o")) {
+    //        $(this).removeClass("fa-square-o").addClass("fa-square");
+    //    }
+    //    else {
+    //        $(this).removeClass("fa-square").addClass("fa-square-o");
+    //    }
+
+    //});
+
     function setLatLng(place, geoLng, geoLat)
     {
         $('#PlaceLongitude').val(geoLng);
