66c1f96cf9d25f3ea41af16b9f501002a3640c37
sashapol <sashapoliacov@gmail.com>
2014-08-16 15:51:54 +0300

    register

--- Interext/Interext/Scripts/site/event-details.js
+++ Interext/Interext/Scripts/site/event-details.js
@@ -6,59 +6,107 @@
 				    label: "Books"
 				},
 				{
-				    value: 50,
-				    color: "#46BFBD",
+				    value: 150,
+				    color: "#02a789",
 				    highlight: "#5AD3D1",
 				    label: "Dogs"
 				},
 				{
-				    value: 100,
-				    color: "#FDB45C",
+				    value: 120,
+				    color: "#bdd84d",
 				    highlight: "#FFC870",
 				    label: "Guitar"
 				},
 				{
-				    value: 40,
-				    color: "#949FB1",
+				    value: 100,
+				    color: "#FFB347",
 				    highlight: "#A8B3C5",
 				    label: "Cooking"
 				},
 				{
-				    value: 120,
-				    color: "#4D5360",
+				    value: 50,
+				    color: "#949FB1",
 				    highlight: "#616774",
 				    label: "Football"
-				}
+				},
+                {
+                    value: 30,
+                    color: "#4D5360",
+                    highlight: "#616774",
+                    label: "Football"
+                }
 
 ];
 
 
+//var doughnutDataInterests = [
+//				{
+//				    value: 300,
+//				    color: "#F7464A",
+//				    highlight: "#FF5A5E",
+//				    label: "Books"
+//				},
+//                {
+//                    value: 150,
+//                    color: "#bdd84d",
+//                    highlight: "#616774",
+//                    label: "Football"
+//                },
+//                {
+//                    value: 120,
+//                    color: "#02a789",
+//                    highlight: "#616774",
+//                    label: "Tracking"
+//                },
+//                {
+//                    value: 100,
+//                    color: "#fee05e",
+//                    highlight: "#FFC870",
+//                    label: "Guitar"
+//                },
+//				{
+//				    value: 50,
+//				    color: "#7db6d3",
+//				    highlight: "#5AD3D1",
+//				    label: "Dogs"
+//				},
+//				{
+//				    value: 40,
+//				    color: "#46BFBD",
+//				    highlight: "#A8B3C5",
+//				    label: "Cooking"
+//				}
+
+
+//];
+
+
 var doughnutDataAge = [
+
+                {
+                    value: 300,
+                    color: "#F7464A",
+                    highlight: "#FF5A5E",
+                    label: "26-30"
+                },
 				{
-				    value: 300,
-				    color: "#F7464A",
-				    highlight: "#FF5A5E",
-				    label: "25-30"
+				    value: 150,
+				    color: "#02a789",
+				    highlight: "#5AD3D1",
+				    label: "16-20"
 				},
 				{
-				    value: 50,
-				    color: "#46BFBD",
-				    highlight: "#5AD3D1",
+				    value: 120,
+				    color: "#bdd84d",
+				    highlight: "#FFC870",
 				    label: "21-25"
 				},
 				{
 				    value: 100,
-				    color: "#FDB45C",
-				    highlight: "#FFC870",
-				    label: "16-20"
-				},
-				{
-				    value: 40,
-				    color: "#949FB1",
+				    color: "#FFB347",
 				    highlight: "#A8B3C5",
 				    label: "35-40"
 				}
-
 ];
 
 var doughnutDataGender = [
@@ -70,7 +118,7 @@ var doughnutDataGender = [
 				},
 				{
 				    value: 70,
-				    color: "#46BFBD",
+				    color: "#02a789",
 				    highlight: "#5AD3D1",
 				    label: "Men"
 				}
