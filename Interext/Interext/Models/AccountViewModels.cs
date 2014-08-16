2a5c5a175c871235f6c39552af6a545a86add667
hilakuker <hilakuker@gmail.com>
2014-08-16 16:45:52 +0300

    change external login confirmation

--- Interext/Interext/Models/AccountViewModels.cs
+++ Interext/Interext/Models/AccountViewModels.cs
@@ -25,8 +25,16 @@ namespace Interext.Models
         [BirthdateValidation]
         public DateTime BirthDate { get; set; }
 
+        [Display(Name = "Image Url")]
+        public string ImageUrl { get; set; }
+
+        [Required]
         [Display(Name = "Gender")]
         public string Gender { get; set; }
+
+        [Display(Name = "Address")]
+        [Required]
+        public string Address { get; set; }
     }
 
     public class ManageUserViewModel
