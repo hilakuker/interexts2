a6ee30579d0d7e10cd0ffb493183918e67c2ae00
sashapol <sashapoliacov@gmail.com>
2014-08-20 11:08:01 +0300

    comit

---
+++ Interext/Interext/Models/Statistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Interext.Models
+{
+    public class Statistics
+    {
+        public List<StatisticItem> Gender { get; set; }
+
+        public List<StatisticItem> Age { get; set; }
+
+        public List<StatisticItem> Interests { get; set; }
+    }
+
+    public class StatisticItem
+    {
+        public int number { get; set; }
+        public string title { get; set; }
+    }
+}
