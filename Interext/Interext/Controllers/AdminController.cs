5fa069e1db73a9e1a98de1b178da7ed631eedb09
hilakuker <hilakuker@gmail.com>
2014-08-23 21:23:36 +0300

    adding admin page (not finished)

---
+++ Interext/Interext/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Interext.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Interext.Controllers
+{
+    [Authorize(Roles = "Admin, AnotherRole")]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var notHandledReports = db.ReportedUrl.Where(x => x.ReportStatus == e_reportStatus.ToBeHandled).ToList();
+            var notHandledReportsView = new List<ReportsViewModel>();
+            foreach (ReportLinks report in notHandledReports)
+            {
+                notHandledReportsView.Add(new ReportsViewModel() 
+                { CreatedTime = report.CreatedTime, Handled = false, ReportedUrl = report.ReportedUrl, Id = report.Id});
+            }
+            return View(notHandledReportsView);
+        }
+        [HttpPost]
+        public ActionResult Index(List<ReportsViewModel> model)
+        {
+            foreach (ReportsViewModel report in model)
+            {
+                if (report.Handled)
+                {
+                    var reportInDb = db.ReportedUrl.Find(report.Id);
+                    reportInDb.ReportStatus = e_reportStatus.Handled;
+                }
+            }
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Interext/Interext/Models/ReportsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Interext.Models
+{
+    public class ReportsViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ReportedUrl { get; set; }
+        [Required]
+        public DateTime CreatedTime { get; set; }
+        public bool Handled { get; set; }
+    }
+}
